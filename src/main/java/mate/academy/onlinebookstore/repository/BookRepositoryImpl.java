3efab82334dcd265e68e5dd70d1a0b9b285f69df
Serhiy <sergie3007@gmail.com>
2025-01-14 17:26:21 +0200

    create book service

---
+++ src/main/java/mate/academy/onlinebookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.onlinebookstore.repository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import mate.academy.onlinebookstore.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cannot add book to DB " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> fromBook = session.createQuery("from Book ", Book.class);
+            return fromBook.getResultList();
+        } catch (Exception e) {
+            throw new NoSuchElementException("Cannot find all from books", e);
+        }
+    }
+}
