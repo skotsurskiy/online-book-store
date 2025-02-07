336bf41385859054ea1b883ea562bfc29bbc0c0c
Serhiy <sergie3007@gmail.com>
2025-02-07 20:51:02 +0200

    add GlobalExceptionHandler

---
+++ src/main/java/mate/academy/onlinebookstore/exception/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package mate.academy.onlinebookstore.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+    private static final String ERRORS = "errors";
+    private static final String ISBN_DUPLICATE_MESSAGE = "isbn must be unique";
+    private static final String WHITESPACE = " ";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex
+    ) {
+        Map<String, Object> body = getDefaultBody(HttpStatus.BAD_REQUEST);
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                .map(this::getErrorMessage)
+                .toList();
+        body.put(ERRORS, errors);
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleDuplicateKeyExceptions() {
+        Map<String, Object> body = getDefaultBody(HttpStatus.BAD_REQUEST);
+        body.put(ERRORS, ISBN_DUPLICATE_MESSAGE);
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleEntityNotFoundExceptions(
+            EntityNotFoundException ex
+    ) {
+        Map<String, Object> body = getDefaultBody(HttpStatus.BAD_REQUEST);
+        body.put(ERRORS, ex.getMessage());
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    private String getErrorMessage(ObjectError objectError) {
+        if (objectError instanceof FieldError fieldError) {
+            String defaultMessage = fieldError.getDefaultMessage();
+            String field = fieldError.getField();
+            return field + WHITESPACE + defaultMessage;
+        }
+        return objectError.getDefaultMessage();
+    }
+
+    private Map<String, Object> getDefaultBody(HttpStatus httpStatus) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, httpStatus);
+        return body;
+    }
+}
