d58e03fb1cf17ce47fbfc9710813192f89537d7f
Serhiy <sergie3007@gmail.com>
2025-02-12 14:24:02 +0200

    book fix

--- src/main/java/mate/academy/onlinebookstore/model/Book.java
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -29,8 +29,7 @@ public class Book {
     @Column(nullable = false)
     private BigDecimal price;
     private String description;
-    @Column(name = "cover_image")
     private String coverImage;
-    @Column(name = "is_deleted", nullable = false)
+    @Column(nullable = false)
     private boolean isDeleted = false;
 }
