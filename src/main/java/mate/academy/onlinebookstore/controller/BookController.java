ddfbb066f16238525f8ca90558790d49b1a62a86
Serhiy <sergie3007@gmail.com>
2025-02-12 13:55:25 +0200

    minor fixes

--- src/main/java/mate/academy/onlinebookstore/controller/BookController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/BookController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Book management", description = "Endpoints for managing books")
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
--- src/main/java/mate/academy/onlinebookstore/model/User.java
+++ src/main/java/mate/academy/onlinebookstore/model/User.java
@@ -25,12 +25,11 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
-    @Column(name = "first_name", nullable = false)
+    @Column(nullable = false)
     private String firstName;
-    @Column(name = "last_name", nullable = false)
+    @Column(nullable = false)
     private String lastName;
-    @Column(name = "shipping_address")
     private String shippingAddress;
-    @Column(name = "is_deleted", nullable = false)
+    @Column(nullable = false)
     private boolean isDeleted = false;
 }
