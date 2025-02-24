8f6096f7ef3799d77b0818cfc5ce3119b898ada2
Serhiy <sergie3007@gmail.com>
2025-02-24 12:30:27 +0200

    fix shoppingCart repository

--- src/main/java/mate/academy/onlinebookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -2,17 +2,14 @@ package mate.academy.onlinebookstore.repository.shoppingcart;
 
 import java.util.Optional;
 import mate.academy.onlinebookstore.model.ShoppingCart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @Query("""
-                from ShoppingCart sc
-                join fetch sc.user u
-                left join fetch sc.cartItems ci
-                left join fetch ci.book
-                where u.id = :id
-            """)
-    Optional<ShoppingCart> getShoppingCartByUserId(@Param("id") Long id);
+    @EntityGraph(attributePaths = {
+            "user",
+            "cartItems",
+            "cartItems.book"
+    })
+    Optional<ShoppingCart> getShoppingCartByUserId(Long id);
 }
